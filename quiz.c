90b6d96f964c5e5568958d5768a8b784385bac4d
artfin <artfin@mail.ru>
2024-01-02 18:26:01 +0300

    restructured main loop

--- quiz.c
+++ quiz.c
@@ -65,6 +65,138 @@ Province* select_random_province()
     return p;
 }
 
+typedef enum {
+    QUIZ = 0,
+    VICTORY,
+    LEARN,
+} GameState; 
+
+Font font = {0};
+Shader shader = {0};
+Image color_image = {0};
+Image black_white_image = {0}; 
+
+typedef struct {
+    Vector2 ul; 
+    Vector2 lr;
+    float width;
+    float height;
+} Rec;
+    
+static size_t error_counter = 0;
+
+void quiz(GameState *state, Rec *rec, Texture *texture, Camera2D *camera, Province **hidden_province)
+{
+    static bool draw_wrong_msg = false;
+
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        Vector2 mouse = GetScreenToWorld2D(GetMousePosition(), *camera);
+
+        printf("mouse.x: %.5lf; mouse.y: %.5lf\n", mouse.x, mouse.y);
+        DrawCircle(mouse.x, mouse.y, 10.0, RED);
+
+        printf("ul_corner: (%.5lf, %.5lf); lr_corner: (%.5lf, %.5lf)\n", rec->ul.x, rec->ul.y, rec->lr.x, rec->lr.y);
+
+        if ( (mouse.x < rec->ul.x) || (mouse.x > rec->lr.x) || (mouse.y < rec->ul.y) || (mouse.y > rec->lr.y)) {
+            printf("Click is outside the image!\n");
+        } 
+        else {
+            int imgx = (int) ( (mouse.x - (GetScreenWidth()/2 - DEFAULT_IMAGE_SCALE*texture->width/2)) / DEFAULT_IMAGE_SCALE);
+            int imgy = (int) ( (mouse.y - (GetScreenHeight()/2 - DEFAULT_IMAGE_SCALE*texture->height/2)) / DEFAULT_IMAGE_SCALE);
+            Color c = GetImageColor(color_image, imgx, imgy);
+            unsigned long hex = rgb_to_hex(c.r, c.g, c.b);
+
+            printf("Click is inside! imgx = %d; imgy = %d; color: (%d, %d, %d, %d) => #%06lx\n", 
+                    imgx, imgy, c.r, c.g, c.b, c.a, hex);
+
+            char* clicked_province = hmget(PROVINCES, hex);
+            if (clicked_province != NULL) {
+                if (strcmp(clicked_province, (*hidden_province)->value) == 0) {
+                    //printf("Province name = %s\n", province_name);
+
+                    for (int px = 0; px < color_image.width; ++px) {
+                        for (int py = 0; py < color_image.height; ++py) {
+                            Color current_color = GetImageColor(color_image, px, py);
+                            if ((current_color.r == c.r) && 
+                                    (current_color.g == c.g) && 
+                                    (current_color.b == c.b) && 
+                                    (current_color.a == c.a)) {
+                                ImageDrawPixel(&black_white_image, px, py, GetColor(PROVINCE_CORRECT_COLOR));
+                            }
+                        }
+                    } 
+
+                    UnloadTexture(*texture);
+                    *texture = LoadTextureFromImage(black_white_image);
+
+                    (*hidden_province)->guessed = true;
+                    *hidden_province = select_random_province();
+
+                    if (*hidden_province == NULL) {
+                        *state = VICTORY;
+                    }
+                } else {
+                    error_counter += 1;
+                    draw_wrong_msg = true;
+                }
+            } else {
+                printf("Province name unknown! Possibly a border has been clicked. \n\n");
+            }
+        }
+    }
+
+    if (*hidden_province != NULL) {
+        Vector2 pos_find_str = CLITERAL(Vector2) {
+            rec->ul.x + 0.03 * DEFAULT_IMAGE_SCALE*texture->width,
+            rec->ul.y + 0.01 * DEFAULT_IMAGE_SCALE*texture->height
+        };
+
+        Vector2 pos_errors_str = CLITERAL(Vector2) {
+            rec->ul.x + 0.75 * DEFAULT_IMAGE_SCALE*texture->width, 
+            rec->ul.y + 0.01 * DEFAULT_IMAGE_SCALE*texture->height
+        };
+
+        BeginShaderMode(shader);
+        DrawTextEx(font, TextFormat("Find '%s'", (*hidden_province)->value), 
+                   pos_find_str, HUD_DEFAULT_FONTSIZE, 0, WHITE);
+        DrawTextEx(font, TextFormat("Error counter: %ld", error_counter), 
+                   pos_errors_str, HUD_DEFAULT_FONTSIZE, 0, WHITE);
+        EndShaderMode();
+    } 
+   
+    static float lifetime_wrong_msg = HUD_LIFETIME; 
+    if (draw_wrong_msg) {
+        float dt = GetFrameTime();
+        lifetime_wrong_msg -= dt;       
+
+        if (lifetime_wrong_msg > 0) {   
+            Vector2 pos = CLITERAL(Vector2) {
+                rec->ul.x + 0.4 * DEFAULT_IMAGE_SCALE*texture->width,
+                rec->ul.y + 0.5 * DEFAULT_IMAGE_SCALE*texture->height 
+            };
+
+            BeginShaderMode(shader);
+            DrawTextEx(font, "Wrong!", pos, HUD_LARGE_FONTSIZE, 0, RED);
+            EndShaderMode();
+        } else {
+            draw_wrong_msg = false;
+            lifetime_wrong_msg = HUD_LIFETIME;
+        }
+    }
+}
+
+void victory(GameState *state, Rec *rec) 
+{
+    Vector2 pos = CLITERAL(Vector2) {
+        rec->ul.x + 0.4 * DEFAULT_IMAGE_SCALE*rec->width,
+        rec->ul.y + 0.5 * DEFAULT_IMAGE_SCALE*rec->height 
+    };
+
+    BeginShaderMode(shader);
+    DrawTextEx(font, "Victory!", pos, HUD_LARGE_FONTSIZE, 0, GREEN);
+    EndShaderMode();
+}
+
 int main()
 {
     srand(time(NULL));
@@ -119,7 +251,6 @@ int main()
     int fileSize = 0;
     unsigned char* fileData = LoadFileData("resources/Alegreya-Regular.ttf", &fileSize);
 
-    Font font = {0};
     font.baseSize = FONT_SIZE_LOAD;
     font.glyphCount = 95;
     font.glyphs = LoadFontData(fileData, fileSize, FONT_SIZE_LOAD, 0, 95, FONT_SDF);
@@ -129,7 +260,7 @@ int main()
 
     UnloadFileData(fileData);
     
-    Shader shader = LoadShader(0, TextFormat("resources/shaders/glsl%i/sdf.fs", GLSL_VERSION));
+    shader = LoadShader(0, TextFormat("resources/shaders/glsl%i/sdf.fs", GLSL_VERSION));
     SetTextureFilter(font.texture, TEXTURE_FILTER_BILINEAR);
 
     RenderTexture2D canvas = LoadRenderTexture(16*factor, 9*factor);
@@ -138,51 +269,57 @@ int main()
     const char* color_map_filename = "resources/mexico-colored.png"; 
     const char* black_white_map_filename = "resources/mexico-black-white.png";
 
-    Image color_image = LoadImage(color_map_filename);
-    Image black_white_image = LoadImage(black_white_map_filename);
+    color_image = LoadImage(color_map_filename);
+    black_white_image = LoadImage(black_white_map_filename);
     
     printf("(color_image) width = %d, height = %d\n", color_image.width, color_image.height);
     printf("(bw_image) width = %d, height = %d\n", black_white_image.width, black_white_image.height);
 
-    Texture2D texture = LoadTextureFromImage(black_white_image);
+    Texture2D map_texture = LoadTextureFromImage(black_white_image);
 
     Province* hidden_province = select_random_province();
     
-    float lifetime_wrong_msg = HUD_LIFETIME; 
-    bool draw_wrong_msg = false;
-    bool draw_victory_msg = false;
+    Camera2D camera = {0};
+    camera.zoom = 1.0; 
 
-    size_t error_counter = 0;
-
-    Camera2D cam = {0};
-    cam.zoom = 1.0; 
+    GameState state = QUIZ;
 
     while (!WindowShouldClose()) {
+        if (IsKeyDown(KEY_R)) {
+            black_white_image = LoadImage(black_white_map_filename);
+            map_texture = LoadTextureFromImage(black_white_image);
+    
+            for (int i = 0; i < hmlen(PROVINCES); ++i) {
+                PROVINCES[i].guessed = false;
+            }
+   
+            error_counter = 0; 
+            hidden_province = select_random_province();
 
+            state = QUIZ;
+        }
+            
         if (IsKeyDown(KEY_S)) {
-            printf("cam.offset.x: %.5lf; cam.offset.y: %.5lf\n", cam.offset.x, cam.offset.y);
-            printf("cam.target.x: %.5lf; cam.target.y: %.5lf\n", cam.target.x, cam.target.y);
-            printf("cam.zoom: %.5lf\n", cam.zoom);
+            printf("cam.offset.x: %.5lf; cam.offset.y: %.5lf\n", camera.offset.x, camera.offset.y);
+            printf("cam.target.x: %.5lf; cam.target.y: %.5lf\n", camera.target.x, camera.target.y);
+            printf("cam.zoom: %.5lf\n", camera.zoom);
         }
 
         if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
             Vector2 delta = GetMouseDelta();
-            delta = Vector2Scale(delta, -1.0f / cam.zoom);
-            cam.target = Vector2Add(cam.target, delta);
+            delta = Vector2Scale(delta, -1.0f / camera.zoom);
+            camera.target = Vector2Add(camera.target, delta);
         }
 
         float wheel = GetMouseWheelMove();
 
         if (wheel != 0) {
-            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), cam);
-            cam.offset = GetMousePosition();
-            cam.zoom += wheel * 0.2f;
-            cam.target = mouseWorldPos;
-            if (cam.zoom < 0.125f) cam.zoom = 0.125f;
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+            camera.offset = GetMousePosition();
+            camera.zoom += wheel * 0.2f;
+            camera.target = mouseWorldPos;
+            if (camera.zoom < 0.125f) camera.zoom = 0.125f;
         }
-
-        int screen_width = GetScreenWidth();
-        int screen_height = GetScreenHeight();
 
         if (IsWindowResized()) {
             UnloadRenderTexture(canvas);
@@ -190,129 +327,41 @@ int main()
         }
 
         BeginTextureMode(canvas); 
-        BeginMode2D(cam);
+        BeginMode2D(camera);
 
         ClearBackground(GetColor(BACKGROUND_COLOR));
-       
+
         // fixed texture position on the screen 
-        float posx = screen_width/2 - DEFAULT_IMAGE_SCALE * texture.width/2;
-        float posy = screen_height/2 - DEFAULT_IMAGE_SCALE * texture.height/2;
-        DrawTextureEx(texture, CLITERAL(Vector2){posx, posy}, 0.0, DEFAULT_IMAGE_SCALE, WHITE);
-            
-        Vector2 ul_corner = CLITERAL(Vector2) {posx, posy}; // upper left corner
-        Vector2 lr_corner = CLITERAL(Vector2) {  // lower right corner
-            posx + DEFAULT_IMAGE_SCALE*texture.width,
-            posy + DEFAULT_IMAGE_SCALE*texture.height
+        float posx = GetScreenWidth()/2 - DEFAULT_IMAGE_SCALE * map_texture.width/2;
+        float posy = GetScreenHeight()/2 - DEFAULT_IMAGE_SCALE * map_texture.height/2;
+        DrawTextureEx(map_texture, CLITERAL(Vector2){posx, posy}, 0.0, DEFAULT_IMAGE_SCALE, WHITE);
+
+        Rec rec = (Rec) {
+            .ul = CLITERAL(Vector2) {posx, posy},
+            .lr = CLITERAL(Vector2) { 
+                posx + DEFAULT_IMAGE_SCALE * map_texture.width,
+                posy + DEFAULT_IMAGE_SCALE * map_texture.height
+            },
+            .width = map_texture.width,
+            .height = map_texture.height
         }; 
-
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            //Vector2 mouse = Vector2Add(GetMousePosition(), cam.target);
-            //printf("mouse.x: %.5lf; mouse.y: %.5lf\n", mouse.x, mouse.y);
-            
-            Vector2 mouse = GetScreenToWorld2D(GetMousePosition(), cam);
-            //mouse = Vector2Subtract(mouse, cam.target);
-
-            printf("mouse.x: %.5lf; mouse.y: %.5lf\n", mouse.x, mouse.y);
-            //DrawCircle(mouse.x, mouse.y, 10.0, RED);
-            
-            printf("ul_corner: (%.5lf, %.5lf); lr_corner: (%.5lf, %.5lf)\n", ul_corner.x, ul_corner.y, lr_corner.x, lr_corner.y);
-
-            if ( (mouse.x < ul_corner.x) || (mouse.x > lr_corner.x) || (mouse.y < ul_corner.y) || (mouse.y > lr_corner.y)) {
-                printf("Click is outside the image!\n");
-            } 
-            else {
-                int imgx = (int) ( (mouse.x - (screen_width/2 - DEFAULT_IMAGE_SCALE*texture.width/2)) / DEFAULT_IMAGE_SCALE);
-                int imgy = (int) ( (mouse.y - (screen_height/2 - DEFAULT_IMAGE_SCALE*texture.height/2)) / DEFAULT_IMAGE_SCALE);
-                Color c = GetImageColor(color_image, imgx, imgy);
-                unsigned long hex = rgb_to_hex(c.r, c.g, c.b);
-                
-                printf("Click is inside! imgx = %d; imgy = %d; color: (%d, %d, %d, %d) => #%06lx\n", 
-                        imgx, imgy, c.r, c.g, c.b, c.a, hex);
-
-                char* clicked_province = hmget(PROVINCES, hex);
-                if (clicked_province != NULL) {
-                    if (strcmp(clicked_province, hidden_province->value) == 0) {
-                        //printf("Province name = %s\n", province_name);
-
-                        for (int px = 0; px < color_image.width; ++px) {
-                            for (int py = 0; py < color_image.height; ++py) {
-                                Color current_color = GetImageColor(color_image, px, py);
-                                if ((current_color.r == c.r) && 
-                                        (current_color.g == c.g) && 
-                                        (current_color.b == c.b) && 
-                                        (current_color.a == c.a)) {
-                                    ImageDrawPixel(&black_white_image, px, py, GetColor(PROVINCE_CORRECT_COLOR));
-                                }
-                            }
-                        } 
-
-                        UnloadTexture(texture);
-                        texture = LoadTextureFromImage(black_white_image);
-
-                        hidden_province->guessed = true;
-                        hidden_province = select_random_province();
-
-                        if (hidden_province == NULL) {
-                            draw_victory_msg = true;
-                        }
-
-                    } else {
-                        error_counter += 1;
-                        draw_wrong_msg = true;
-                    }
-                } else {
-                    printf("Province name unknown! Possibly a border has been clicked. \n\n");
-                }
+ 
+        switch (state) 
+        {
+            case QUIZ: {
+                quiz(&state, &rec, &map_texture, &camera, &hidden_province);
+                break;
             }
-        }
 
-        if (hidden_province != NULL) {
-            Vector2 pos_find_str = CLITERAL(Vector2) {
-                ul_corner.x + 0.03 * DEFAULT_IMAGE_SCALE*texture.width,
-                ul_corner.y + 0.01 * DEFAULT_IMAGE_SCALE*texture.height
-            };
-            
-            Vector2 pos_errors_str = CLITERAL(Vector2) {
-                ul_corner.x + 0.75 * DEFAULT_IMAGE_SCALE*texture.width, 
-                ul_corner.y + 0.01 * DEFAULT_IMAGE_SCALE*texture.height
-            };
-            
-            BeginShaderMode(shader);
-                DrawTextEx(font, TextFormat("Find '%s'", hidden_province->value), 
-                           pos_find_str, HUD_DEFAULT_FONTSIZE, 0, WHITE);
-                DrawTextEx(font, TextFormat("Error counter: %ld", error_counter), 
-                           pos_errors_str, HUD_DEFAULT_FONTSIZE, 0, WHITE);
-            EndShaderMode();
-        } 
-
-        if (draw_wrong_msg) {
-            float dt = GetFrameTime();
-            lifetime_wrong_msg -= dt;       
-
-            if (lifetime_wrong_msg > 0) {   
-                Vector2 pos = CLITERAL(Vector2) {
-                    ul_corner.x + 0.4 * DEFAULT_IMAGE_SCALE*texture.width,
-                    ul_corner.y + 0.5 * DEFAULT_IMAGE_SCALE*texture.height 
-                };
-
-                BeginShaderMode(shader);
-                    DrawTextEx(font, "Wrong!", pos, HUD_LARGE_FONTSIZE, 0, RED);
-                EndShaderMode();
-            } else {
-                draw_wrong_msg = false;
-                lifetime_wrong_msg = HUD_LIFETIME;
+            case VICTORY: {
+                victory(&state, &rec);
+                break;
             }
-        }
 
-        if (draw_victory_msg) {
-            Vector2 pos = CLITERAL(Vector2) {
-                ul_corner.x + 0.4 * DEFAULT_IMAGE_SCALE*texture.width,
-                ul_corner.y + 0.5 * DEFAULT_IMAGE_SCALE*texture.height 
-            };
-
-            BeginShaderMode(shader);
-                DrawTextEx(font, "Victory!", pos, HUD_LARGE_FONTSIZE, 0, GREEN);
-            EndShaderMode();
+            case LEARN: {
+                assert(false);
+                break;
+            }
         }
 
         EndMode2D();
@@ -322,7 +371,7 @@ int main()
         // flip texture 
         DrawTexturePro(canvas.texture,
             CLITERAL(Rectangle){0, 0, canvas.texture.width, -canvas.texture.height },
-            CLITERAL(Rectangle){0, 0, screen_width, screen_height}, 
+            CLITERAL(Rectangle){0, 0, GetScreenWidth(), GetScreenHeight()}, 
             CLITERAL(Vector2) {0, 0},
             0, WHITE);
                 
@@ -330,7 +379,7 @@ int main()
     } 
 
     UnloadFont(font);    
-    UnloadTexture(texture); 
+    UnloadTexture(map_texture); 
     UnloadImage(color_image);
     UnloadImage(black_white_image);
     UnloadRenderTexture(canvas);
