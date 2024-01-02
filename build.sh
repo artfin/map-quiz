cc1651589a493287117e4ca6cb59a1a39282adb6
artfin <artfin@mail.ru>
2024-01-02 18:25:39 +0300

    add missing build flags

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ CC=gcc
 
 CFLAGS="-ggdb -Wall"
 INC="-I./raylib/include"
-LIB="-L./raylib/lib -lraylib -lm"
+LIB="-L./raylib/lib -lraylib -lm -ldl -lpthread"
 
 set -xe
 $CC $CFLAGS $INC quiz.c -o quiz $LIB
